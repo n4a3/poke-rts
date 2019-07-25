63e7221ca5492d5f43743bf7a12bb7485a44ad70
Aleksandr Krasnikov <krasnikov@ittest18.mercury.office>
2019-07-25 17:20:03 +0400

    gh docs added

---
+++ docs/precache-manifest.4610fc1dcfefd43e0fa03b83f1a963f9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31596f0333dc2075963a9f4aee80bf33",
+    "url": "/poke-rts/index.html"
+  },
+  {
+    "revision": "6c5004f11edb7b6b201c",
+    "url": "/poke-rts/static/js/2.3a83dad0.chunk.js"
+  },
+  {
+    "revision": "6ecb54c197d7a069b1d1",
+    "url": "/poke-rts/static/js/main.450792ed.chunk.js"
+  },
+  {
+    "revision": "542119caa7accc1d1d9c",
+    "url": "/poke-rts/static/js/runtime~main.52339882.js"
+  }
+]);
